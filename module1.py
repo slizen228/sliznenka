72872009cf25f97fa3ed73d2b53573c6f2e79780
slizen <kukusha33333@mail.ru>
2020-01-19 13:02:56 +0300

    commiit

---
+++ module1.py
@@ -0,0 +1,10 @@
+f=open('Perepis.txt','r')
+l=list()
+s=str()
+s1=str()
+for line in f:
+    s=line
+    s1=s[-5:-1:]
+    if int(s1)<1978:
+        print(line)
+
---
+++ module2.py
@@ -0,0 +1,12 @@
+f=open('Perepis.txt','r')
+l=list()
+s=str()
+s1=str()
+n=int(input("укажите menshee znachenie"))
+b=int(input("укажите bolshee znachenie"))
+for line in f:
+    s=line
+    s1=s[-5:-1:]
+    if n<int(s1)<b:
+        print(line)
+
